7b228afdec509309af5b756960ece202b28cf87c
carlenee <carleneannabel@gmail.com>
2022-11-21 01:37:31 +0700

    aa

--- lib/pages/watch_list_page.dart
+++ lib/pages/watch_list_page.dart
@@ -15,7 +15,7 @@ class WatchListPage extends StatefulWidget {
 }
 
 class _WatchListPageState extends State<WatchListPage> {
-  List _watchlist = [];
+  // List _watchlist = [];
   // @override
   // void initState() {
   //   super.initState();
@@ -23,40 +23,19 @@ class _WatchListPageState extends State<WatchListPage> {
   //   mywatchlist = getWatchList(context);
   // }
 
-  void initState() {
-    getWatchList();
-  }
-
-  Future<void> getWatchList() async {
-    final String response =
-        await rootBundle.loadString('assets/watchlist.json');
-    final data = json.decode(response);
-    setState(() {
-      _watchlist = data["items"];
-    });
-  }
-  // Future<List<WatchList>> fetchWatchList() async {
-  //   var url = Uri.parse(
-  //       'https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10');
-  //   var response = await http.get(
-  //     url,
-  //     headers: {
-  //       "Access-Control-Allow-Origin": "*",
-  //       "Content-Type": "application/json",
-  //     },
-  //   );
-
-  //   var data = jsonDecode(utf8.decode(response.bodyBytes));
-
-  //   List<WatchList> listWatchList = [];
-  //   for (var d in data) {
-  //     if (d != null) {
-  //       listWatchList.add(WatchList.fromJson(d));
-  //     }
-  //   }
-
-  //   return listWatchList;
+  // void initState() {
+  //   getWatchList();
   // }
+
+  // Future<void> getWatchList() async {
+  //   final String response =
+  //       await rootBundle.loadString('assets/watchlist.json');
+  //   final data = json.decode(response);
+  //   setState(() {
+  //     _watchlist = data["items"];
+  //   });
+  // }
+  
 
   @override
   Widget build(Object context) {
@@ -75,96 +54,97 @@ class _WatchListPageState extends State<WatchListPage> {
         ),
       ),
       drawer: DrawerClass("My Watch List"),
-      // body: FutureBuilder(
-      //     future: getWatchList(),
-      //     builder: (context, AsyncSnapshot snapshot) {
-      //       if (snapshot.data == null) {
-      //         return const Center(child: CircularProgressIndicator());
-      //       } else {
-      //         if (!snapshot.hasData) {
-      //           return Column(
-      //             children: const [
-      //               Text(
-      //                 "Tidak ada watch list :(",
-      //                 style:
-      //                     TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-      //               ),
-      //               SizedBox(height: 8),
-      //             ],
-      //           );
-      //         } else {
-      //           return ListView.builder(
-      //               itemCount: snapshot.data!.length,
-      //               itemBuilder: (_, index) => Container(
-      //                     margin: const EdgeInsets.symmetric(
-      //                         horizontal: 16, vertical: 12),
-      //                     padding: const EdgeInsets.all(20.0),
-      //                     decoration: BoxDecoration(
-      //                         color: Colors.white,
-      //                         borderRadius: BorderRadius.circular(15.0),
-      //                         boxShadow: const [
-      //                           BoxShadow(
-      //                               color: Colors.black, blurRadius: 2.0)
-      //                         ]),
-      //                     child: Column(
-      //                       mainAxisAlignment: MainAxisAlignment.start,
-      //                       crossAxisAlignment: CrossAxisAlignment.start,
-      //                       children: [
-      //                         Text(
-      //                           "${snapshot.data![index].fields.title}",
-      //                           style: const TextStyle(
-      //                             fontSize: 18.0,
-      //                             fontWeight: FontWeight.bold,
-      //                           ),
-      //                         ),
-      //                         // const SizedBox(height: 10),
-      //                         // Text("${snapshot.data![index].completed}"),
-      //                       ],
-      //                     ),
-      //                   ));
-      //         }
-      //       }
-      //     })
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Column(
-          children: [
-            // Display the data loaded from sample.json
-            _watchlist.isNotEmpty
-                ? Expanded(
-                    child: ListView.builder(
-                      itemCount: _watchlist.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                            key: ValueKey(_watchlist[index]["pk"]),
-                            margin: const EdgeInsets.all(10),
-                            color: Color(0xFFB2DFDB),
-                            child: ListTile(
-                              title: Text(
-                                  _watchlist[index]["fields"]["title"]),
-                              onTap: () => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => WatchListDetailPage(
-                                            title: _watchlist[index]["fields"]
-                                                ["title"],
-                                            watched: _watchlist[index]["fields"]
-                                                ["watched"],
-                                            rating: _watchlist[index]["fields"]
-                                                ["rating"],
-                                            releasedDate: _watchlist[index]
-                                                ["fields"]["release_date"],
-                                            review: _watchlist[index]["fields"]
-                                                ["review"],
-                                          ))),
-                            ));
-                      },
+      body: FutureBuilder(
+          // future: fetchMyWatchList(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              if (!snapshot.hasData) {
+                return Column(
+                  children: const [
+                    Text(
+                      "Tidak ada watch list :(",
+                      style:
+                          TextStyle(color: Color(0xff59A5D8), fontSize: 20),
                     ),
-                  )
-                : Container()
-          ],
-        ),
-      ),
+                    SizedBox(height: 8),
+                  ],
+                );
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (_, index) => Container(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 12),
+                          padding: const EdgeInsets.all(20.0),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15.0),
+                              boxShadow: const [
+                                BoxShadow(
+                                    color: Colors.black, blurRadius: 2.0)
+                              ]),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "${snapshot.data![index].fields.title}",
+                                style: const TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              // const SizedBox(height: 10),
+                              // Text("${snapshot.data![index].completed}"),
+                            ],
+                          ),
+                        ));
+              }
+            }
+          }
+        )
+      // body: Padding(
+      //   padding: const EdgeInsets.all(25),
+      //   child: Column(
+      //     children: [
+      //       // Display the data loaded from sample.json
+      //       _watchlist.isNotEmpty
+      //           ? Expanded(
+      //               child: ListView.builder(
+      //                 itemCount: _watchlist.length,
+      //                 itemBuilder: (context, index) {
+      //                   return Card(
+      //                       key: ValueKey(_watchlist[index]["pk"]),
+      //                       margin: const EdgeInsets.all(10),
+      //                       color: Color(0xFFB2DFDB),
+      //                       child: ListTile(
+      //                         title: Text(
+      //                             _watchlist[index]["fields"]["title"]),
+      //                         onTap: () => Navigator.push(
+      //                             context,
+      //                             MaterialPageRoute(
+      //                                 builder: (context) => WatchListDetailPage(
+      //                                       title: _watchlist[index]["fields"]
+      //                                           ["title"],
+      //                                       watched: _watchlist[index]["fields"]
+      //                                           ["watched"],
+      //                                       rating: _watchlist[index]["fields"]
+      //                                           ["rating"],
+      //                                       releasedDate: _watchlist[index]
+      //                                           ["fields"]["release_date"],
+      //                                       review: _watchlist[index]["fields"]
+      //                                           ["review"],
+      //                                     ))),
+      //                       ));
+      //                 },
+      //               ),
+      //             )
+      //           : Container()
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
